a9a958ef08d00969877e4a2b61ea459592623940
S.P. ROY <sprithi44@gmail.com>
2023-03-22 11:47:06 -0400

    userRoutes.js find user by email and match password

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { User } = require('../../models');
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+    const validPassword = await userData.checkPassword(req.body.password);
+
+  }
+});
