5629485e679c34a12f352f670c1c886c34a5b368
S.P. ROY <sprithi44@gmail.com>
2023-03-22 12:08:28 -0400

    resturantRoutes get restaurants by id

--- controllers/api/restaurantRoutes.js
+++ controllers/api/restaurantRoutes.js
@@ -10,3 +10,18 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// GET a single restaurant by id
+router.get('/:id', async (req, res) => {
+  try {
+    const restaurantData = await Restaurant.findByPk(req.params.id);
+    if (!restaurantData) {
+      res.status(404).json({ message: 'No restaurant found with this id!' });
+      return;
+    }
+    res.status(200).json(restaurantData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
