7d34e61c66d0bade2e549f0a0fa580ef74652b87
S.P. ROY <sprithi44@gmail.com>
2023-03-23 11:46:37 -0400

    profile handlebar route added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,30 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const restaurantRoutes = require("./restaurantRoutes");
+const db = require('../../models');
 
 router.use("/users", userRoutes);
 router.use("/restaurants", restaurantRoutes);
+
+exports.getMatches = async (req, res) => {
+  try {
+    // Get the user's location and food preferences from the request query parameters
+    const { location, food_preferences } = req.query;
+
+    // Find all users that have the same location and food preferences as the current user
+    const matches = await db.User.findAll({
+      where: {
+        location,
+        food_preferences
+      }
+    });
+
+    // Return the matches as JSON data
+    res.json(matches);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+};
 
 module.exports = router;
