1532872df5e03cb03c41cdcaf6d6937499df8481
moviefan322 <philman202@yahoo.com>
2023-03-23 19:25:55 -0400

    added more restaraunts to DB

--- seeds/seed.js
+++ seeds/seed.js
@@ -2,11 +2,17 @@ const sequelize = require('../config/connection');
 const { User, Restaurant, Date, Message } = require('../models');
 
 const rawRestaurantData = require('./restaurantData.json');
+const rawRestaurantData2 = require('./restaurantData2.json');
 const userData = require('./userData.json');
 const datesData = require('./dateData.json');
 const messageData = require('./messageData.json');
 
 const filteredData = rawRestaurantData.filter(
+  (
+    { dba, boro, cuisine_description, latitude, longitude } // eslint-disable-line
+  ) => dba && boro && cuisine_description && latitude && longitude // eslint-disable-line
+);
+const filteredData2 = rawRestaurantData2.filter(
   (
     { dba, boro, cuisine_description, latitude, longitude } // eslint-disable-line
   ) => dba && boro && cuisine_description && latitude && longitude // eslint-disable-line
@@ -21,6 +27,10 @@ const seedDatabase = async () => {
   });
 
   await Restaurant.bulkCreate(filteredData, {
+    returning: true,
+  });
+
+  await Restaurant.bulkCreate(filteredData2, {
     returning: true,
   });
 
