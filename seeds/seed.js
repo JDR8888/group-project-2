b3c75d1685c98812ad059d778b260f9e0c22059c
moviefan322 <philman202@yahoo.com>
2023-03-24 00:30:22 -0400

    trying to grow db

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,18 +1,12 @@
 const sequelize = require('../config/connection');
 const { User, Restaurant, Date, Message } = require('../models');
 
-const rawRestaurantData = require('./restaurantData.json');
-const rawRestaurantData2 = require('./restaurantData2.json');
+const rawRestaurantData01 = require('./newList.json');
 const userData = require('./userData.json');
 const datesData = require('./dateData.json');
 const messageData = require('./messageData.json');
 
-const filteredData = rawRestaurantData.filter(
-  (
-    { dba, boro, cuisine_description, latitude, longitude } // eslint-disable-line
-  ) => dba && boro && cuisine_description && latitude && longitude // eslint-disable-line
-);
-const filteredData2 = rawRestaurantData2.filter(
+const filteredData = rawRestaurantData01.filter(
   (
     { dba, boro, cuisine_description, latitude, longitude } // eslint-disable-line
   ) => dba && boro && cuisine_description && latitude && longitude // eslint-disable-line
@@ -27,10 +21,7 @@ const seedDatabase = async () => {
   });
 
   await Restaurant.bulkCreate(filteredData, {
-    returning: true,
-  });
-
-  await Restaurant.bulkCreate(filteredData2, {
+    individualHooks: true,
     returning: true,
   });
 
