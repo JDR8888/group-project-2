45c12d1893c55e5f9381618e0675c186e2180222
jake rudlong <jrudlong@gmail.com>
2023-03-24 21:01:45 -0500

    confirms put route works from dating page modal

--- public/js/update.js
+++ public/js/update.js
@@ -2,12 +2,16 @@
 const updateFormHanlder = async (event) => {
     event.preventDefault();
     const location = (document.getElementById('location').value);
-    const what_to_eat = (document.getElementById('what-to-eat').value); // es-lint ignore-line
+    const what_to_eat = (document.getElementById('what-to-eat').value); 
     console.log(location, what_to_eat);
-    fetch("api/users", {
+    const response = await fetch("api/users", {
         method: "PUT",
-        body: JSON.stringify(location, what_to_eat)
-    })
+        body: JSON.stringify({location, what_to_eat}), headers: { "Content-Type": "application/json"},
+    });
+    console.log(response);
+    if (response.ok){
+    // document.location.reload();
+    };
 };
 
 document.querySelector('#update-btn').addEventListener('click', updateFormHanlder);
