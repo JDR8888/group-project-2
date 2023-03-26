e00cf2151f1655a5b8aa1eb12a71a74464a56e25
moviefan322 <philman202@yahoo.com>
2023-03-26 12:35:12 -0400

    adds user_id to homepage for message check

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,6 +7,7 @@ router.get('/', async (req, res) => {
     res.render('homepage', {
       title: 'Homepage',
       logged_in: req.session.logged_in,
+      user_id: req.session.user.id,
     });
   } catch (err) {
     res.status(500).json(err);
